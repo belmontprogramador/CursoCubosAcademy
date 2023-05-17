9cb91d0cbb702f74ea53438d3fece552a168a840
Felipe Belmont <belmontprogramador@gmail.com>
2023-05-16 22:57:16 -0300

    exercicos durante a aula dois

---
+++ aula2.js
@@ -0,0 +1,26 @@
+// Exercicios de Variavel
+
+const cidadeNatal = "Araruama";
+console.log(`A minha cidade natal é ${cidadeNatal}`);
+
+let populacaoDeAraruma = 134.293;
+console.log(`A população de Araruama é de ${populacaoDeAraruma}`);
+
+populacaoDeAraruma = 164.852;
+console.log(`A população de Araruama é de ${populacaoDeAraruma}`);
+
+let x = 10,
+  y = 2;
+let resultado = x + y;
+console.log(`A soma de ${x} + ${y} é igual ${resultado}`);
+
+resultado = x - y;
+console.log(`A subtração de ${x} + ${y} é igual ${resultado}`);
+
+resultado = x * y;
+console.log(`A multiplicação de ${x} + ${y} é igual ${resultado}`);
+
+resultado = x / y;
+console.log(`A divisão de ${x} + ${y} é igual ${resultado}`);
+
+ 
